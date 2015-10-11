c20575ba4420be8527459cdc6210fd6b7e279040
Eric Knapik <eric11mk@gmail.com>
2015-10-11 14:53:47 -0700

    Initial Commit

---
+++ SimpleRayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleRayTracer
+//
+//  Created by Eric Knapik on 10/11/15.
+//  Copyright © 2015 EKnapik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
