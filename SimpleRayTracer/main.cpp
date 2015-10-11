cf664aaf2a2be4bd8471c592924f69d604190bd5
Eric Knapik <eric11mk@gmail.com>
2015-10-11 16:05:42 -0700

    Initial Commit

--- SimpleRayTracer/main.cpp
+++ SimpleRayTracer/main.cpp
@@ -6,10 +6,65 @@
 //  Copyright © 2015 EKnapik. All rights reserved.
 //
 
+#include <GLUT/GLUT.h>
+#include <OpenGL/gl.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define WINDOW_HEIGHT 512
+#define WINDOW_WIDTH 512
+
+void initOpenGL(void);
+void display(void);
+
+int main(int argc, char * argv[]) {
+    
+    // Setup window statically because GLUT is poor at window and display render
+    glutInit( &argc, argv );
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    
+    glutInitWindowSize(WINDOW_HEIGHT, WINDOW_WIDTH);
+    glutCreateWindow("Simple Ray Tracer");
+    glutDisplayFunc(display);
+    
+    glutMainLoop();
     return 0;
 }
+
+
+void initOpenGL(void) {
+    glEnable(GL_DEPTH_TEST | GL_CULL_FACE | GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor( 0.0, 0.0, 0.0, 1.0 ); // Black
+    glCullFace(GL_FRONT);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    
+    return;
+}
+
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // poll events
+    // bind and draw
+    
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+
+/*
+ Ideally there would be a main overal program that creates the scene of objects
+ The rays would then shoot into the scene and color pixels
+ 
+ Create window:
+ Create scene
+ Create rayTracer that takes a scene
+    Create two triangles and draws to the screne
+    Create a texture from raytraced pixels what to draw
+        Simple Shader of setting texture CPU processing
+    Create a specific shader for the scene all hard coded
+ Create a Display function <- hard coded to the window connection
+    Can Take a rayTracer to use.
+
+*/
