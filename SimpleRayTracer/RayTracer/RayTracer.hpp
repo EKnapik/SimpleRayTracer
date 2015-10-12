716ad1379dd2c86de2bc1c4bc95a63a8f09b8356
Eric Knapik <eric11mk@gmail.com>
2015-10-11 22:40:11 -0700

    Added the TODO for the ray tracer class.

--- SimpleRayTracer/RayTracer/RayTracer.hpp
+++ SimpleRayTracer/RayTracer/RayTracer.hpp
@@ -26,3 +26,26 @@ private:
 
 
 #endif /* RayTracer_hpp */
+
+/* TODO
+ * Set function to use per pixel including default for constructor which can be
+    given one to start with
+ * Needs to be given the window dimensions to be create the 3D array
+ * Needs the scene to shoot rays into. Scene must have:
+    object info
+    camera info
+ * Spawns threads to determine some particular pixel then collects and sets
+    thread returned info to the 3D array
+ * Setup the OpenGL code for:
+    create 2 triangles that cover screne
+    create a texture from 3D array
+    pass texture to OpenGL
+    compile and use the simple necessary shader code
+        might want to create a shader object to use here
+  * render function:
+    The manager basically.. at this time in the scene what does it lool like and
+    display that to the GLUT window. then in the future the GLFW window I am
+    given.
+ 
+ 
+ */
