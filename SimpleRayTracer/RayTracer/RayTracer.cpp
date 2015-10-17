3bf5bba77b679a1c910a4b4c4174c411365d87d4
Eric Knapik <eric11mk@gmail.com>
2015-10-16 21:37:02 -0700

    Outlined the render to window

--- SimpleRayTracer/RayTracer/RayTracer.cpp
+++ SimpleRayTracer/RayTracer/RayTracer.cpp
@@ -48,6 +48,28 @@ void RayTracer::setupOpenGLCalls(void) {
 	vertPos = glGetAttribLocation(shaderProgram, "currVert");
 	uvPos = glGetAttribLocation(shaderProgram, "uvVert");
 	// some call for the texture location
+}
+
+void RayTracer::renderToWindow(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eBuffer);
+	glUseProgram(shaderProgram);
+
+	// pass the vertex data info
+	int step = 5*sizeof(GLfloat);
+	glEnableVertexAttribArray(vertPos);
+	glVertexAttribPointer(vertPos, 3, GL_FLOAT, step, 0);
+
+	glEnableVertexAttribArray(uvPos);
+	glVertexAttribPointer(uvPos, 2, GL_FLOAT, step, 3*sizeof(GLfloat));
+
+	// Gonna need to pass that texture over
+	//
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, elementData);
+	glutSwapBuffers();
+}
 
 
 void RayTracer::changeScene(Scene *newScene) {
