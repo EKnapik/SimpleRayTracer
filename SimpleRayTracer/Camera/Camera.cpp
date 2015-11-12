dde808ab9ef5349dbaa8067266daf271db8a7956
Eric Knapik <eric11mk@gmail.com>
2015-11-11 17:37:07 -0800

    Small Camera class functionality. Need to write vec3, vec4, mat3 operation overloaders.

--- SimpleRayTracer/Camera/Camera.cpp
+++ SimpleRayTracer/Camera/Camera.cpp
@@ -7,3 +7,37 @@
 //
 
 #include "Camera.hpp"
+
+
+Camera::Camera(Vec3 *pos, Vec3 *lookAtPoint, float roll) {
+    this->pos = pos;
+    this->lookAtPoint = lookAtPoint;
+    this->roll = roll;
+    this->focalLen = 1.0;
+    createCamMatrix();
+}
+
+
+Camera::Camera(Vec3 *pos, Vec3 *lookAtPoint) {
+    this->pos = pos;
+    this->lookAtPoint = lookAtPoint;
+    this->roll = 0.0;
+    this->focalLen = 1.0;
+    createCamMatrix();
+}
+
+
+Vec3* Camera::getRayDir(int row, int col) {
+    //vec3 rayDir = camMat * normalize(vec3(p.xy, focalLen));
+    return new Vec3(0.0);
+}
+
+
+
+// Updates the camera matrix with the current position and lookAtPoint of the
+// Camera
+void Camera::createCamMatrix() {
+    Vec3 *cw, *cp, *cu, *cv;
+    
+    
+}
