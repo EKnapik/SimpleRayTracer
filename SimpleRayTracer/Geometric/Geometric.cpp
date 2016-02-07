c575fcbfeddfdd74203e6750690cb6ac01ad3b32
Eric Knapik <eric11mk@gmail.com>
2016-02-06 23:42:53 -0500

    Added new file

---
+++ SimpleRayTracer/Geometric/Geometric.cpp
@@ -0,0 +1,9 @@
+//
+//  Geometric.cpp
+//  SimpleRayTracer
+//
+//  Created by Eric Knapik on 2/6/16.
+//  Copyright © 2016 EKnapik. All rights reserved.
+//
+
+#include "Geometric.hpp"
---
+++ SimpleRayTracer/Geometric/Geometric.hpp
@@ -0,0 +1,14 @@
+//
+//  Geometric.hpp
+//  SimpleRayTracer
+//
+//  Created by Eric Knapik on 2/6/16.
+//  Copyright © 2016 EKnapik. All rights reserved.
+//
+
+#ifndef Geometric_hpp
+#define Geometric_hpp
+
+#include <stdio.h>
+
+#endif /* Geometric_hpp */
